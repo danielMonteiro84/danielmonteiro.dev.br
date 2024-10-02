229cba4e2e4afef2301f03835b0097d73da8cf01
Daniel Monteiro <158519099+danielMonteiro84@users.noreply.github.com>
2024-10-02 23:01:56 +0000

    Modificando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Futuro portfólio Daniel Monteiro - Desenvolvedor FullStack</h1>;
+  return <h1>Futuro portfólio Daniel Monteiro - Engenheiro de Software</h1>;
 }
 
 export default Home;
